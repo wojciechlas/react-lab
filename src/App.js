d375e31a819a247d45b4675368e1060472ffc231
Wojciech Las <w.las@live.com>
2025-01-16 20:44:51 +0100

    Reaktywność

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  let title = 'Wall-E';
+  const [title, setTitle] = useState('Wall-E');
 
   function handleChange(event) {
-      console.log(event.target.value);
+      setTitle(event.target.value);
+  }
+
+  let message;
+
+  if (title.length < 5) {
+    message = 'Tytuł jest za krótki.';
+  } else if (title.length < 15) {
+    message = 'Tytuł jest w sam raz na plakat przed kinem!';
+  } else {
+    message = 'Tytuł jest za długi, nikt tego nei zapamięta.';
   }
 
   return (
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
-          <input type="text" onChange={handleChange}/>
+          {
+            title.length > 0 && <h3>{message}</h3>
+          }
+          <input type="text" value={title} onChange={handleChange}/>
       </div>
   );
 }
