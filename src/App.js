c9c18e2801357ddb78f44376c98a452ab9fde865
wojciechlas <w.las@live.com>
2025-01-16 20:19:47 +0100

    Komponenty / JSX

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = 'Wall-E';
+
+  function handleChange(event) {
+      console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>My favourite movies to watch</h1>
+          <h2>My favourite movie for today is {title}</h2>
+          <input type="text" onChange={handleChange}/>
+      </div>
   );
 }
 
